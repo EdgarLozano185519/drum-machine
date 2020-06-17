eeaabfa7f8b3481f54457c8379f6a3d85ef80ae4
Edgar Lozano <edgar@edgarl.com>
2020-06-17 18:11:00 -0500

    Updates

---
+++ precache-manifest.5f92c15dfbb6ba428a9d1a69e8296c42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98364d7ed47c947d58e4b54e940e62a4",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "9dd6bba71364e9cccf74",
+    "url": "/drum-machine/static/css/main.7b071f5e.chunk.css"
+  },
+  {
+    "revision": "2a53c971c7858e638584",
+    "url": "/drum-machine/static/js/2.51eb61d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.51eb61d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd6bba71364e9cccf74",
+    "url": "/drum-machine/static/js/main.f2c4e9ee.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
